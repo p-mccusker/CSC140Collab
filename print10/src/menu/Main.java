f8e22921a9ec313d3040bd7947f76ecd8d1c31f1
Peter McCusker <petermcc2000@yahoo.com>
2019-01-29 09:23:57 -0600

    Added Skel

---
+++ print10/src/menu/Main.java
@@ -0,0 +1,24 @@
+package menu;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		System.out.println("1. ");
+		System.out.println("2. ");
+		System.out.println("3. ");
+		System.out.println("4. ");
+		System.out.println("5. ");
+		System.out.println("6. ");
+		System.out.println("7. ");
+		System.out.println("8. ");
+		System.out.println("9. ");
+		System.out.println("10. ");
+		System.out.println("Choose an option: ");
+	}
+
+}
